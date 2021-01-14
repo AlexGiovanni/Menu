6822def003de49fb1a78ccd28996c651c4172053
unknown <alexis.santiago01@utrgv.edu>
2021-01-13 22:19:01 -0600

    made categories dynamic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories =['all',...new Set(items.map((item)=> item.category))] ;
+console.log(allCategories);
+
 function App() {
   const [menuItems,setMenuItems]=useState(items);
-  const [categories,setCategories]=useState([]);
+  const [categories,setCategories]=useState(allCategories);
 
   const filterItems = (category)=>{
     if(category === 'all'){
-      setMenuItems(items)
+      setMenuItems(items);
       return;
     }
 
@@ -25,7 +28,7 @@ function App() {
           <div className='underline'></div>
         </div>
 
-        <Categories filterItems={filterItems}/>
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu items={menuItems}/>
       </section>
     </main>
